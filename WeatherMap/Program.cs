c95abfa5f0abc2b593a9d29a1d5ff952ae00a071
Jake Johnston <jakejohnston618@gmail.com>
2023-04-22 13:36:46 -0500

    finished weather map

---
+++ WeatherMap/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode.");
+            string zipCode = Console.ReadLine();
+
+            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}&units=imperial";
+
+            Console.WriteLine();
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F");
+        }
+    }
+}
---
+++ WeatherMap/WeatherMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherMap
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+        }
+    }
+}
